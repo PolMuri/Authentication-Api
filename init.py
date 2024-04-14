facad50e1f8265e22afbbf84ceadcf2c1813b50a
PolMuri <pmpolriuda9@gmail.com>
2024-04-14 11:52:15 +0200

    Afegits comentaris més precisos

--- init.py
+++ init.py
@@ -1,15 +1,15 @@
-# Importa las funciones necesarias de otros archivos
+# Importem les funcions necessàries d'altres fitxers
 from core import update_user_token
 from db import load_data, save_data
 
-# Define la ruta de la base de datos
+# Definim la ruta de la BD
 rutaDB = "db.json"
 
-# Define una función para inicializar la contraseña
+# Definim una funció per inicialitzar la contrasenya
 def initialize_password(token, password):
-    # Carga los datos actuales de la base de datos
+    # Carreguem les dades actuals de la BD
     data = load_data(rutaDB)
-    # Actualiza la información del usuario con el token y la contraseña proporcionados
+    # Actualitzem la informació de l'usuari amb el token i la contrasenya proporcionats
     update_user_token(data, token, password)
-    # Guarda los datos actualizados en la base de datos
+    # Desem les dades actualitzades a la base de dades
     save_data(data, rutaDB)
